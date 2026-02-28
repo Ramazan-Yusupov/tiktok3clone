1aa16bd3b991100f5af25c6e1d1d05ab2e5958d2
Frontend <frontenddev747@gmail.com>
2026-02-28 14:13:22 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/tiktok3/",
+  base: "/tiktok3clone/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
